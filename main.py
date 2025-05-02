bf3b82bef315d04ef08b58b3c93f0ee3714a1922
Ilia Zorin <iivzorin@gmail.com>
2025-05-02 19:19:21 +0300

    test_2

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 class MainClass:
     def get_local_number(self):
         return 14
+
+    def __init__(self):
+        self._class_number = 46
+
+    def get_class_number(self):
+        return self._class_number
--- test.py
+++ test.py
@@ -2,6 +2,12 @@ from main import MainClass
 
 class TestMainClass:
     def test_get_local_number(self):
-        obj = MainClass()
-        result = obj.get_local_number()
+        main_class = MainClass()
+        result = main_class.get_local_number()
         assert result == 14, "Ошибка: метод get_local_number должен возвращать число 14"
+
+    def test_get_class_number(self):
+        main_class = MainClass()
+        result = main_class.get_class_number()
+
+        assert result > 45, "Ошибка: метод get_class_number должен возвращать число больше 45"
